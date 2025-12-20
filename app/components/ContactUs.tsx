47b5f756da26f18042599fee1456d694166995e4
hongyangme <canxi925@gmail.com>
2025-12-21 07:18:45 +0900

    feat: ContactUs 컴포넌트 레이아웃 수정 및 스타일 개선

--- app/components/ContactUs.tsx
+++ app/components/ContactUs.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { motion } from 'motion/react';
 import toast from 'react-hot-toast';
 import { Toaster } from 'react-hot-toast';
-import Title from './Title';
 
 const ContactUs = () => {
   const onSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
@@ -36,7 +35,7 @@ const ContactUs = () => {
       <Toaster position="bottom-center" reverseOrder={false} />
       <div className="framer-ur7txx" data-framer-name="섹션5: 문의하기">
         <div className="framer-kd1ens flex-col justify-between xl:flex-row">
-          <div className="mb-4">
+          <div className="mb-6">
             <h2 className="font-instrument text-[28px] leading-[0.8] font-bold md:text-[48px] xl:text-[72px]">
               Contact
             </h2>
@@ -96,12 +95,14 @@ const ContactUs = () => {
             </div>
 
             <div>
-              <div className="text-balance">
+              <div className="my-3 text-xs text-pretty md:text-base xl:text-lg">
                 <div className="">
                   <p>
-                    <strong>-외주 편집, 자비 출판 관련 문의</strong>
+                    <strong className="text-[#5E5E5E]">
+                      - 외주 편집, 자비 출판 관련 문의
+                    </strong>
                   </p>
-                  <p>
+                  <p className="text-[#A1A1A1]">
                     도서 장르, 제목, 원고(예상)분량, 원고 완성도, 원고 구성
                     요소(EX: 텍스트, 사진, 삽화, 도표, 음원 유무 및 대략의 개수)
                     필수 기재
@@ -109,9 +110,11 @@ const ContactUs = () => {
                 </div>
                 <div className="mt-4">
                   <p>
-                    <strong>-출판 코칭 관련 문의</strong>
+                    <strong className="text-[#5E5E5E]">
+                      - 출판 코칭 관련 문의
+                    </strong>
                   </p>
-                  <p>
+                  <p className="text-[#A1A1A1]">
                     코칭 목적 (EX: 개인 소장용 도서 출간, 출판사 투고 및 계약
                     관련, 출판사 취업(창업) 준비 등) 필수 기재
                   </p>
@@ -119,7 +122,10 @@ const ContactUs = () => {
               </div>
             </div>
 
-            <button type="submit" className="framer-3F5nR framer-1kzllwq">
+            <button
+              type="submit"
+              className="framer-3F5nR framer-1kzllwq cursor-pointer"
+            >
               <p className="text-base text-white md:text-lg lg:text-[21px]">
                 출판 상담 문의하기
               </p>
